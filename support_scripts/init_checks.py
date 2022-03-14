f8cf9c912d9345396cdf7327a122beb23eb9e3fa
Axel Hörteborn <axel.n.c.andersson@gmail.com>
2022-03-14 11:38:15 +0100

    upgraded to 2.8.0

--- support_scripts/init_checks.py
+++ support_scripts/init_checks.py
@@ -21,7 +21,7 @@ def check_and_install_requirements():
         except ModuleNotFoundError:
             QMessageBox.information(None, 'ERROR',
                                     """During the first startup this program there are some third party packages that is required to be installed, 
-GeoDataFarm tried to install them automatic but failed. You can try to manually install the two packages with "pip install matplotlib", "pip install reportlab", "pip install cython"
+GeoDataFarm tried to install them automatic but failed. You can try to manually install the two packages with "pip install matplotlib", "pip install reportlab", "pip install cython", "pip install pandas"
 (If you are using Windows you need to run it from the OSGeo4W shell) 
 If can't get the plugin to work, don't hesitate to send an e-mail to geodatafarm@gmail.com and tell which os you are using and QGIS version.""")
             sys.exit()
@@ -39,10 +39,30 @@ If can't get the plugin to work, don't hesitate to send an e-mail to geodatafarm
         except ModuleNotFoundError:
             QMessageBox.information(None, 'ERROR',
                                     """During the first startup this program there are some third party packages that is required to be installed, 
-GeoDataFarm tried to install them automatic but failed. You can try to manually install the two packages with "pip install matplotlib", "pip install reportlab", "pip install cython"
+GeoDataFarm tried to install them automatic but failed. You can try to manually install the two packages with "pip install matplotlib", "pip install reportlab", "pip install cython", "pip install pandas"
 (If you are using Windows you need to run it from the OSGeo4W shell) 
 If can't get the plugin to work, don't hesitate to send an e-mail to geodatafarm@gmail.com and tell which os you are using and QGIS version.""")
             sys.exit()
+
+    try:
+        import pandas
+    except ModuleNotFoundError:
+        print('installing pandas')
+        if platform.system() == 'Windows':
+            subprocess.call([sys.exec_prefix + '/python', "-m", 'pip', 'install', 'pandas'])
+        else:
+            subprocess.call(['python3', '-m', 'pip', 'install', 'pandas'])
+        try:
+            import pandas
+            print('installation completed')
+        except ModuleNotFoundError:
+            QMessageBox.information(None, 'ERROR',
+                                    """During the first startup this program there are some third party packages that is required to be installed, 
+GeoDataFarm tried to install them automatic but failed. You can try to manually install the two packages with "pip install matplotlib", "pip install reportlab", "pip install cython", "pip install pandas"
+(If you are using Windows you need to run it from the OSGeo4W shell) 
+If can't get the plugin to work, don't hesitate to send an e-mail to geodatafarm@gmail.com and tell which os you are using and QGIS version.""")
+            sys.exit()
+
 
 def check_if_pyagri_is_built():
     try:
