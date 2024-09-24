6b4d8edfaae6c95b35f0ddf9adca29583013b059
Axel Hörteborn <geodatafarm@gmail.com>
2024-09-24 07:12:20 +0200

    fixed_removeal_datasets

--- tests/test_xclose.py
+++ tests/test_xclose.py
@@ -7,13 +7,13 @@ def test_remove_crop(gdf: GeoDataFarm):
     gdf.dock_widget.PBRemoveCrop.click()
 
 # @pytest.mark.depends(on=['import_text'], name='remove_text')
-def test_remove_dataset(gdf: GeoDataFarm):
+def test_remove_test_field_datasets(gdf: GeoDataFarm):
     gdf.dock_widget.PBEditTables.click()
     items = [gdf.tabel_mgmt.TMD.SATables.item(i).text() for i in range(gdf.tabel_mgmt.TMD.SATables.count())]
     found = False
     for i, text in enumerate(items):
         if 'test_field' in text:
-            gdf.tabel_mgmt.TMD.SATables.itemAt(i, 0).setCheckState(2)
+            gdf.tabel_mgmt.TMD.SATables.item(i).setCheckState(2)
             found = True
     gdf.tabel_mgmt.TMD.pButRemove.click()
     assert found
@@ -25,14 +25,14 @@ def test_remove_iso_dataset(gdf: GeoDataFarm):
     found = False
     for i, text in enumerate(items):
         if 'test_iso_field' in text:
-            gdf.tabel_mgmt.TMD.SATables.itemAt(i, 0).setCheckState(2)
+            gdf.tabel_mgmt.TMD.SATables.item(i).setCheckState(2)
             found = True
     gdf.tabel_mgmt.TMD.pButRemove.click()
     assert found
 fields = ['test_field', 'test_iso_field'] #, 'test_iso_field2']
 # @pytest.mark.depends(scope='session', on=['remove_text'], name='remove_field')
 @pytest.mark.parametrize('field_name', fields)
-def test_remove_field(gdf: GeoDataFarm, field_name):
+def test_remove_xfield(gdf: GeoDataFarm, field_name):
     gdf.add_field.clicked_define_field()
     items = [gdf.dock_widget.LWFields.item(i).text() for i in range(gdf.dock_widget.LWFields.count())]
     for i, text in enumerate(items):
