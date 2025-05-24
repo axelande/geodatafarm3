51fc993da4ad0f017342c6d1075603a834fb9a7e
Axel Hörteborn <geodatafarm@gmail.com>
2025-05-24 09:21:45 +0200

    updated test

--- tests/test_k_analyse_data.py
+++ tests/test_k_analyse_data.py
@@ -16,5 +16,5 @@ def test_analyse_consistency(gdf: GeoDataFarm):
 def test_analyse_yield(gdf: GeoDataFarm):
     names = [["plant", "test_field_plant_2023_04_15"],["harvest", "test_field_harvest_2023_09_15"]]
     analyse = Analyze(gdf, names)
-    d = analyse.get_initial_distinct_values('yield_kg__ha__', 'test_field_harvest_2023_09_15', 'harvest')
+    d = analyse.get_initial_distinct_values('yield_kg__per_ha__', 'test_field_harvest_2023_09_15', 'harvest')
     assert len(d["distinct_values"]) == 2256
